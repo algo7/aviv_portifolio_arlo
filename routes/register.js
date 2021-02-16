be12bf20a0bebd5d376f0d45b112a3098031c086
thealgorithm <11154774+algo7@users.noreply.github.com>
2021-02-16 21:14:03 +0800

    refactor the login logout controller

--- routes/register.js
+++ routes/register.js
@@ -1,14 +1,14 @@
-//Dependencies
+// Dependencies
 const express = require('express');
 const router = express.Router();
 const passport = require('passport');
 const { emptyInputCheck, } = require('express-suite');
 // const { registerFunc, } = require('../config/auth/register');
 
-//Redis
+// Redis
 const { client, } = require('../config/dataBase/redisConnection');
 
-//Empty Input Check Config
+// Empty Input Check Config
 router.use(emptyInputCheck({
     checkGet: false,
     emptyBodyMsg: 'Empty Request',
@@ -16,11 +16,11 @@ router.use(emptyInputCheck({
     supressFieldKey: false,
 }));
 
-//GET Routes
-//Login
+// GET Routes
+// Login
 router.get('/login', (req, res) => {
 
-    // Redirect the user to the home page if the user is already logged in
+    //  Redirect the user to the home page if the user is already logged in
     if (req.user) {
         return res.redirect('/');
     }
@@ -28,111 +28,95 @@ router.get('/login', (req, res) => {
     res.render('auth/login', { layout: 'id_based', });
 });
 
-//Log Out
+// Log Out
 router.get('/out', (req, res) => {
 
-    //Delete the session key in redis
+    // Delete the session key in redis
     client.del(`sess:${req.sessionID}`);
 
-    //Count redis keys
-    client.keys('*', (err) => {
-
-        if (err) {
-            res.send(500).json({
-                msg: 'Logout Error',
-            });
-            return;
-        }
-    });
-    console.info('User Logged Out');
-
-    //Express Logout func
+    // Express Logout func
     req.logOut();
     req.session.destroy();
 
-    //Back to the login page
+    console.info('User Logged Out');
+
+    // Back to the login page
     res.redirect('/');
 
 });
 
 
-//POST Routes
-//Login
+// POST Routes
+// Login
 router.post('/login', (req, res, next) => {
 
-    //Using passport-local
+    // Using passport-local
     passport.authenticate('local', (err, user) => {
 
-        //Check for error (the user's verification status)
+        // Check for error (the user's verification status)
         if (err) {
-            return res.status(401)
-                .json({
-                    msg: 'Error Comparing Password',
-                });
+            return res.status(401).json({ msg: 'Error Comparing Password', });
         }
 
-        //If user object does not exist => login failed
+        // If user object does not exist => login failed
         if (!user) {
-            return res.status(401)
-                .json({
-                    msg: 'No Such User',
-                });
+            return res.status(401).send('No Such User');
         }
 
-        //If all good, log the dude in
+        // If all good, log the dude in
         req.logIn(user, (err) => {
+
             if (err) {
-                return res.status(401)
-                    .json({
-                        msg: 'Login Error',
-                    });
+                console.error(err);
+                return res.status(401).send('Login Error');
             }
 
-            res.redirect('/');
-            return;
+            return res.redirect('/');
+
         });
+
     })(req, res, next);
 });
 
-//Locked Routes
-//Register
-// router.get('/reg', (req, res) => {
-//     res.render('auth/register', { layout: 'id_based', });
-// });
+// Locked Routes
+// Register
+//  router.get('/reg', (req, res) => {
+//      res.render('auth/register', { layout: 'id_based', });
+//  });
 
-//Register
-// router.post('/reg', (req, res) => {
+// Register
+//  router.post('/reg', (req, res) => {
 
-//     //Extract the request body
-//     const { body, } = req;
+//      // Extract the request body
+//      const { body, } = req;
 
-//     // The registeration function
-//     registerFunc(
-//         body.firstName,
-//         body.lastName,
-//         body.password,
-//         body.passwordC,
-//         body.email
-//         birthDay: body.birthDay,
-//         birthName: body.birthName,
-//         location: body.location,
-//         phone: body.phone,
-//         interests: body.interests,
-//         study: body.study,
-//         degree: body.degree,
-//         bio: body.bio,
-//     )
-//         .then(result => {
-//             //If the error array is there
-//             if (result) {
-//                 res.send(result);
-//                 return;
-//             }
-//             res.sendStatus(200);
-//         })
-//         .catch(err => { console.log(err); });
-// });
+//      //  The registeration function
+//      registerFunc(
+//          body.firstName,
+//          body.lastName,
+//          body.password,
+//          body.passwordC,
+//          body.email
+//          birthDay: body.birthDay,
+//          birthName: body.birthName,
+//          location: body.location,
+//          phone: body.phone,
+//          interests: body.interests,
+//          study: body.study,
+//          degree: body.degree,
+//          bio: body.bio,
+//      )
+//          .then(result => {
+//              // If the error array is there
+//              if (result) {
+//                  res.send(result);
+//                  return;
+//              }
+//              res.sendStatus(200);
+//          })
+//          .catch(err => { console.log(err); });
+//  });
 
 
-//Export the Module
+// Export the Module
 module.exports = router;
